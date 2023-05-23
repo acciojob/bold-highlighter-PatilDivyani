06e8838961bc6686d8f3137914925b911753b6c7
PatilDivyani <125538181+PatilDivyani@users.noreply.github.com>
2023-05-23 19:35:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 function highlight() {
     //Write your code here
-
+    for(let i = 0; i<boldText.length; i++){
+        boldText[i].style.color = "green";
+    }
 
 }
 
 
 function return_normal() {
     //Write your code here
-
+    for(let i = 0; i<boldText.length; i++){
+        boldText[i].style.color = "black";
+    }
     
 }
+
+//let boldText = document.getElementsByTagName("p")[1].children;
+let boldText = document.getElementsByTagName("strong");
